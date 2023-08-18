b7dcdf1ec03112e0e4d5f0a2d7eef4c32ded4177
bmbaji <bmbaji@ufl.edu>
2023-08-18 19:08:19 +0100

    quiz app

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,91 @@
+export const  Questions = [
+    {
+        prompt:"What is 1 + 1?",
+        optionA: "24",
+        optionB: "2",
+        optionC: "8",
+        optionD: "4",
+        answer: "B"
+    },
+    {
+        prompt: "What is the capital of France?",
+        optionA: "London",
+        optionB: "Paris",
+        optionC: "Berlin",
+        optionD: "Rome",
+        answer: "B"
+        },
+        {
+        prompt: "Who painted the Mona Lisa?",
+        optionA: "Vincent van Gogh",
+        optionB: "Pablo Picasso",
+        optionC: "Leonardo da Vinci",
+        optionD: "Michelangelo",
+        answer: "C"
+        },
+        {
+            prompt: "What is the largest planet in our solar system?",
+            optionA: "Jupiter",
+            optionB: "Saturn",
+            optionC: "Earth",
+            optionD: "Mars",
+            answer: "A"
+            },
+            {
+            prompt: "What is the chemical symbol for gold?",
+            optionA: "Ag",
+            optionB: "Au",
+            optionC: "Fe",
+            optionD: "Hg",
+            answer: "B"
+            },
+            {
+            prompt: "Who wrote the play 'Romeo and Juliet'?",
+            optionA: "William Shakespeare",
+            optionB: "George Bernard Shaw",
+            optionC: "Jane Austen",
+            optionD: "Charles Dickens",
+            answer: "A"
+            },
+            {
+                prompt: "What is the tallest mountain in the world?",
+                optionA: "Mount Everest",
+                optionB: "K2",
+                optionC: "Mount Kilimanjaro",
+                optionD: "Mount McKinley",
+                answer: "A"
+                },
+                {
+                prompt: "Which planet is known as the Red Planet?",
+                optionA: "Jupiter",
+                optionB: "Mars",
+                optionC: "Mercury",
+                optionD: "Venus",
+                answer: "B"
+                },
+                {
+                prompt: "What is the chemical symbol for water?",
+                optionA: "H2O",
+                optionB: "CO2",
+                optionC: "NaCl",
+                optionD: "O2",
+                answer: "A"
+                },
+                {
+                    prompt: "Who invented the telephone?",
+                    optionA: "Alexander Graham Bell",
+                    optionB: "Thomas Edison",
+                    optionC: "Nikola Tesla",
+                    optionD: "Albert Einstein",
+                    answer: "A"
+                    },
+                    {
+                    prompt: "Which country is home to the kangaroo?",
+                    optionA: "Australia",
+                    optionB: "Canada",
+                    optionC: "Mexico",
+                    optionD: "Brazil",
+                    answer: "A"
+                    }
+
+]
